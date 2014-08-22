55556c0e49a526fb921c91457bed737ea195916c
lok117 <lok117@LOK117-01>
2014-08-22 10:41:51 +0200

    index

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Databank_oefening index</title>
+        <link href="Databank_oefening.css" type="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+            <header class="main_header">
+
+            </header>
+            <div class="body_cont">
+                <div class="main_left">
+                    
+                </div>
+                <div class="main_mid">
+                    <ul class="lijst">
+                        <li>
+                            <a href="http://localhost/Databank_oefeningen/Verbinding_maken.php">Verbinding maken</a>
+                        </li>
+                        <li>
+                            <a href="http://localhost/Databank_oefeningen/Gegevens_ophalen.php">Gegevens ophalen</a>
+                        </li>
+                        <li>
+                            <a href="http://localhost/Databank_oefeningen/Gegevens_ophalen.php">Gegevens ophalen</a>
+                        </li>
+                    </ul>
+                    <?php
+                    // put your code here
+                    ?>
+                </div>
+                <div class="main_right">
+                    
+                </div>
+            </div>
+            <footer class="main_footer">
+
+            </footer>
+        </div>
+    </body>
+</html>
